a21c35630a4a444015d446edc614b8ba16354fdf
wwwtttjjj <1019495562@qq.com>
2022-11-23 18:45:38 +0800

    first time

---
+++ main.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import time
+import random
+import numpy as np
+from itertools import cycle
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader
+
+from unet import UNet
+from utils import data_loading, ramps, losses, dice_score
+import tqdm
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_path', type=str, default='./data/train', help='the path of training data')
+parser.add_argument('--val_path', type=str, default='./data/val', help='the path of val data')
+parser.add_argument('--save_path', type=str, default='./checkpoint', help='the path of save_model')
+parser.add_argument('--deterministic', type=int,  default=1, help='whether use deterministic training')
+parser.add_argument('--max_iterations', type=int,  default=6000, help='maximum epoch number to train')
+
+parser.add_argument('--seed', type=int,  default=1337, help='random seed')
+parser.add_argument('--gpu', type=int,  default=0, help='GPU to use')
+parser.add_argument('--base_lr', type=float,  default=0.01, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type = int, default=1, help = 'the batch_size of training size')
+parser.add_argument('--ema_decay', type=float,  default=0.99, help='ema_decay')
+parser.add_argument('--consistency', type=float,  default=0.1, help='consistency')
+parser.add_argument('--consistency_rampup', type=float,  default=40.0, help='consistency_rampup')
+args = parser.parse_args()
+
+train_path = args.train_path
+labeled_path = train_path + '/' + "labelded_data"
+weak_path = train_path + '/' + "weak_labeled_data"
+
+os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+batch_size = args.batch_size
+max_iterations = args.max_iterations
+base_lr = args.base_lr
+num_classes = 4
+datasize = (400, 400)
+
+#保证可重复性
+if args.deterministic:
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+def get_current_consistency_weight(epoch):
+# Consistency ramp-up from https://arxiv.org/abs/1610.02242
+    return args.consistency * ramps.sigmoid_rampup(epoch, args.consistency_rampup)
+
+def update_ema_variables(model, ema_model, alpha, global_step):
+    
+    # Use the true average until the exponential average is more correct
+    alpha = min(1 - 1 / (global_step + 1), alpha)
+    for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+        ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
+
+def create_model(ema=False):
+    # Network definition
+    net = UNet(n_channels=1, n_classes=num_classes)
+    model = net.cuda()
+    #截断反向传播的梯度流
+    if ema:
+        for param in model.parameters():
+            param.detach_()
+    return model
+
+model = create_model()
+ema_model = create_model(ema=True)
+'''data'''
+def worker_init_fn(worker_id):
+    random.seed(args.seed+worker_id)
+labeled_dataset = data_loading.BasicDataset(imgs_dir=labeled_path+'/' + 'imgs', masks_dir=labeled_path+'/'+'masks',size=datasize)
+weak_dataset = data_loading.BasicDataset(imgs_dir=weak_path+'/'+'imgs', masks_dir=weak_path+'/'+'masks', probability_dir=weak_path+'/'+'probability_maps', size=datasize)
+labeled_dataloader = DataLoader(dataset=labeled_dataset, batch_size=batch_size, shuffle=True, num_workers=0,pin_memory=True,worker_init_fn=worker_init_fn)
+weak_dataloader = DataLoader(dataset=weak_dataset, batch_size=batch_size*2, shuffle=True, num_workers=0,pin_memory=True,worker_init_fn=worker_init_fn)
+
+'''some para'''
+model.train()
+ema_model.train()
+optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
+consistency_criterion = losses.softmax_mse_loss
+max_epoch = max_iterations//len(weak_dataloader)+1
+CEloss = torch.nn.CrossEntropyLoss()
+iter_num = 0
+'''train'''
+#nclos自定义长度
+for epoch_num in tqdm(range(max_epoch), ncols=70):
+    for i, sampled_batch in enumerate(zip(labeled_dataloader, cycle(weak_dataloader))):
+        #labeled data and weak labeded data
+        labeled_imgs, labeled_masks = sampled_batch[0]['image'], sampled_batch[0]['mask']
+        weak_imgs, weak_masks, p_maps = sampled_batch[1]['image'], sampled_batch[1]['mask'], sampled_batch[1]['probability_map']
+        #to gpu
+        labeled_imgs, labeled_masks = labeled_imgs.cuda(), labeled_masks.cuda()
+        weak_imgs, weak_masks, p_maps = weak_imgs.cuda(), weak_masks.cuda(), p_maps.cuda()
+        #噪声
+        noise = torch.clamp(torch.randn_like(weak_imgs) * 0.1, -0.2, 0.2)
+        #前向传播（model and emamodel）
+        ema_inputs = weak_imgs + noise
+        outputs = model(labeled_imgs)
+        with torch.no_grad():
+            ema_output = ema_model(ema_inputs)
+
+        #计算有监督损失
+        supervised_loss = CEloss(outputs, labeled_masks) + dice_score.dice_loss(F.softmax(outputs, dim=1).float(),
+                    F.one_hot(labeled_masks, num_classes).permute(0, 3, 1, 2).float(),
+                    multiclass=True)
+
+        # loss_seg = F.cross_entropy(outputs, labeled_masks)
+        # outputs_soft = F.softmax(outputs, dim=1)
+        #一致性损失
+        consistency_weight = get_current_consistency_weight(iter_num//150)
+        consistency_dist = consistency_criterion(outputs, ema_output)
+        consistency_loss = consistency_weight * consistency_dist
+        loss = 0.5*(supervised_loss) + consistency_loss
+        '''一大块留着计算弱监督的损失'''
+        #student model反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        #teacher model EMA更新参数
+        update_ema_variables(model, ema_model, args.ema_decay, iter_num)
+        iter_num = iter_num + 1
+
+        ## change lr
+        if iter_num % 2500 == 0:
+            lr_ = base_lr * 0.1 ** (iter_num // 2500)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr_
+        #迭代1k保存模型
+        if iter_num % 1000 == 0:
+            save_mode_path = os.path.join(args.save_path, 'iter_' + str(iter_num) + '.pth')
+            torch.save(model.state_dict(), save_mode_path)
+        if iter_num >= max_iterations:
+            break
+    if iter_num >= max_iterations:
+        break
+save_mode_path = os.path.join(args.save_path, 'iter_'+str(max_iterations)+'.pth')
+torch.save(model.state_dict(), save_mode_path)
---
+++ utils/data_loading.py
@@ -0,0 +1,105 @@
+from os.path import splitext
+from os import listdir
+import numpy as np
+from glob import glob
+import torch
+from torch.utils.data import Dataset
+import logging
+from PIL import Image
+
+
+# from utils.dataGenerator import sample_random_patch_png, sample_label_with_coor_png
+
+
+# 将文件夹里的数据读成dataset
+class BasicDataset(Dataset):
+    # 构造函数
+    def __init__(self, imgs_dir, masks_dir,size, probability_dir = None):
+        self.size = size  # 图像统一裁剪尺寸
+        self.imgs_dir = imgs_dir  # 图像文件夹
+        self.masks_dir = masks_dir  # 标签文件夹
+        self.probability_dir = probability_dir
+
+        # 获得所有图像文件名
+        self.ids = [file for file in listdir(imgs_dir)
+                    if not file.startswith('.')]
+        # logging.info(f'Creating dataset with {len(self.ids)} examples')
+
+    # dataset长度函数
+    def __len__(self):
+        return len(self.ids)
+
+    # 数据预处理
+    @classmethod
+    def preprocess(cls, pil_img, is_img, size):
+        # 如果是图像对象，将它转成numpy数组
+        if is_img:
+            img_nd = np.array(pil_img)
+        else:
+            img_nd = pil_img
+
+        # 如果是一通道图像，将它转成3通道
+        if len(img_nd.shape) == 2:
+            img_nd = np.expand_dims(img_nd, axis=2)
+
+        img_trans = img_nd
+        # 将图像的像素值归一化到0到1之间
+        if img_trans.max() > 1:
+            img_trans = img_trans / 255
+
+        # 将图像裁剪为统一大小
+        if len(size) > 0:
+            x = size[1]  # 获取统一裁剪x轴方向上的长度
+            y = size[0]  # 获取统一裁剪y轴方向上的长度
+            jiany = int((img_trans.shape[1] - y + 10) / 2)  # 计算y轴方向两侧需要裁减掉的距离
+            jianx = int((img_trans.shape[0] - x + 10) / 2)  # 计算x轴方向两侧需要裁减掉的距离
+
+            # 因为裁减掉的长度不一定被2整除，所以判断各种情况来进行左右填充
+            if (img_trans.shape[0] - x) % 2 == 1 and (img_trans.shape[1] - y) % 2 == 1:
+                img_trans = img_trans[jianx + 1:-1 * jianx, jiany + 1:-1 * jiany, :]
+            elif (img_trans.shape[0] - x) % 2 == 1:
+                img_trans = img_trans[jianx + 1:-1 * jianx, jiany:-1 * jiany, :]
+            elif (img_trans.shape[1] - y) % 2 == 1:
+                img_trans = img_trans[jianx:-1 * jianx, jiany + 1:-1 * jiany, :]
+            else:
+                img_trans = img_trans[jianx:-1 * jianx, jiany:-1 * jiany, :]
+
+        return img_trans  # 返回处理后的图像numpy数组
+
+    # dataset返回对应位置数据函数
+    def __getitem__(self, i):
+        idx = self.ids[i]  # 获取第i个图像的文件名
+        mask_file = self.masks_dir + idx[:-4] + '.png'  # 获取第i个标签的文件路径
+        img_file = self.imgs_dir + idx  # 获取第i个图像的文件路径
+
+
+        mask_original = np.asarray(Image.open(mask_file))  # 读取标签
+        mask = mask_original.copy()  # 有时候没有修改权限，需要复制一份
+        mask = mask / 255  # 将mask转为0、1标签
+
+
+        img = Image.open(img_file)  # 读取图像
+
+        img = self.preprocess(img, 1, self.size)  # 图像预处理
+        mask = self.preprocess(mask, 0, self.size)  # 标签预处理
+
+        idx_un = list(np.unique(mask))
+        for i in range(len(idx_un)):
+            mask[mask == idx_un[i]] = i
+
+        if self.probability_dir:
+            npy_file = np.load(self.probability_dir + idx[:-4] + '.npy')
+            probability_map = npy_file.copy()
+            probability_map = probability_map / 10 #npy file保存的概率，值在0-10之间
+            probability_map = self.preprocess(probability_map, 0, self.size)
+
+            return {
+                'image': torch.from_numpy(img.transpose((2, 0, 1))).type(torch.FloatTensor),  # 返回tensor类型的图像
+                'mask': torch.from_numpy(mask.transpose((2, 0, 1))).type(torch.FloatTensor),  # 返回tensor类型的标签
+                'probability_map':torch.from_numpy(probability_map.transpose((2, 0, 1))).type(torch.FloatTensor)#返回概率图谱
+            }
+
+        return {
+            'image': torch.from_numpy(img.transpose((2, 0, 1))).type(torch.FloatTensor),  # 返回tensor类型的图像
+            'mask': torch.from_numpy(mask.transpose((2, 0, 1))).type(torch.FloatTensor)  # 返回tensor类型的标签
+        }
